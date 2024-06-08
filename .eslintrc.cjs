b5ab3b5b1d05697202690f307e8e398f67de2d27
M4cr0s3 <tmmllllllllll@gmail.com>
2024-06-09 00:07:22 +0300

    fix: fixed .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'dev-dist', 'node_modules', '/src/components/ui/**'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
